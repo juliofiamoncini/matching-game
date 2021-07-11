80b400272b487b8ffb1013e94468ba2df6544396
juliofiamoncini <juliofiamoncini@gmail.com>
2021-07-11 12:16:36 -0300

    Create PlayerName component

---
+++ src/components/PlayerName/index.js
@@ -0,0 +1,11 @@
+import "./style.css"
+
+function PlayerName(name) {
+    return `
+        <article class="player-name">
+            ${name}
+        </article>
+    `;
+}
+
+export default PlayerName;
